e186880c082b23173bfed6812ec45eaa80495927
Ivan Figurnov <56535597+darkjohn13@users.noreply.github.com>
2022-12-20 04:17:03 +0500

    HW3 preparations (lesson)

--- src/gameScreen.js
+++ src/gameScreen.js
@@ -1,4 +1,4 @@
-function renderGameScreen(){
+function renderGameScreen() {
     const h1 = document.createElement('h1');
     h1.textContent = 'здесь будет экран игры';
     app.appendChild(h1);
--- src/startScreen.js
+++ src/startScreen.js
@@ -1,16 +1,16 @@
-function renderStartBlock(){
+function renderStartBlock() {
     app.appendChild(templateEngine(startScreenTemplate));
-
 
     const diffButtons = document.querySelector('.start-screen__diff-holder');
     diffButtons.addEventListener('click', (event) => {
-
         event.preventDefault();
         const target = event.target;
 
-        if (target.tagName === "BUTTON"){
+        if (target.tagName === 'BUTTON') {
             window.application.gameDifficulty = target.textContent;
-            Array.from(diffButtons.children).forEach(child => child.classList.remove('start-screen__diff-changer_current'));
+            Array.from(diffButtons.children).forEach((child) =>
+                child.classList.remove('start-screen__diff-changer_current')
+            );
             target.classList.add('start-screen__diff-changer_current');
         }
     });
@@ -23,17 +23,15 @@ function renderStartBlock(){
 }
 window.application.blocks['startBlock'] = renderStartBlock;
 
-function renderStartScreen(){
-   window.application.renderBlock('startBlock', app);
+function renderStartScreen() {
+    window.application.renderBlock('startBlock', app);
 }
 window.application.screens['startScreen'] = renderStartScreen;
-
 
 const startScreenTemplate = {
     tag: 'div',
     cls: 'start-screen',
-    content:
-    [
+    content: [
         {
             tag: 'div',
             cls: 'start-screen__title',
@@ -42,17 +40,18 @@ const startScreenTemplate = {
         {
             tag: 'form',
             cls: 'start-screen__form',
-            content: 
-            [
+            content: [
                 {
                     tag: 'div',
                     cls: 'start-screen__diff-holder',
-                    content:
-                    [
-                        {  
+                    content: [
+                        {
                             tag: 'button',
-                            cls: ['start-screen__diff-changer', 'start-screen__diff-changer_current'],
-                            content: '1',                       
+                            cls: [
+                                'start-screen__diff-changer',
+                                'start-screen__diff-changer_current',
+                            ],
+                            content: '1',
                         },
                         {
                             tag: 'button',
@@ -64,18 +63,16 @@ const startScreenTemplate = {
                             cls: 'start-screen__diff-changer',
                             content: '3',
                         },
-                    ]
+                    ],
                 },
                 {
                     tag: 'button',
                     cls: 'start-screen__button',
-                    content: 'Старт'
-                }
-            ]
-        }
-    ]
-}
-
-
+                    content: 'Старт',
+                },
+            ],
+        },
+    ],
+};
 
 window.application.renderScreen('startScreen');
